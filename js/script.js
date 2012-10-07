d2a1717bc7ff5de3223f34c00632af4ec59b4223
Wilkins Fernandez <wilkinsf@gmail.com>
2012-10-07 18:51:05 -0400

    init

---
+++ js/script.js
@@ -0,0 +1,67 @@
+(function($) {
+    $("#tabs").tabs();
+    $("body").fadeIn();
+
+    $("#close").click(function() {
+        $("#notice").effect("explode");
+    });
+
+    $('tr:even').addClass('alt');
+    $('td').each(function() {
+        $(this).attr("valign", "top");
+    });
+    $("tr").hover(function() {
+        $(this).addClass('over');
+    }, function() {
+        $(this).removeClass('over');
+    });
+
+    // Examples
+    $("#default_usage").counter();
+    $("#default_usage_num_only").counter({
+        'text' : false
+    });
+    $("#charUp").counter({
+        count : 'up',
+        goal : 10
+    });
+    $("#wordDown").counter({
+        type : 'word',
+        goal : 20
+    });
+
+    $("#wordUp").counter({
+        type : 'word',
+        goal : 20,
+        count : 'up'
+    });
+
+    $("#custom_msg").counter({
+        msg : 'words left before you fall into a pit of emptiness.'
+    });
+
+    $("#keepCountingChar").counter({
+        goal : 'sky'
+    });
+
+    $("#keepCountingWord").counter({
+        goal : 'sky',
+        type : 'word',
+        msg : 'amazing words'
+    });
+
+    $("#translate_words").counter({
+        goal : 10,
+        type : 'word',
+        translation : 'caracter palavra restante m&agrave;x'
+    });
+    $("#translate_char").counter({
+        goal : 10,
+        type : 'word',
+        count : 'up',
+        translation : 'caracter palavra restante m&agrave;x'
+    });
+    
+    $("a[target^='_blank']").append("<span class='ui-icon ui-icon-newwin'>&nbsp;</span>");
+
+})(jQuery); 
